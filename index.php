ce8d9b87e849ae3ded1d7f6a83831b699599473c
Henrique Dantas <henrique543@gmail.com>
2023-01-13 15:49:35 -0300

    adicionei o modal para adicionar tarefas

--- index.php
+++ index.php
@@ -48,6 +48,40 @@ if (isset($_SESSION['id']) && empty($_SESSION['id']) == false) {
         </div>
     </nav>
 
+    <!--adicionar tarefa-->
+    <!-- Button trigger modal -->
+    <div class="display-flex">
+        <button type="button" class="btn btn-primary m-4" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Adicionar tarefa
+        </button>
+        <button type="button" class="btn btn-primary m-3">
+            Excluir todas as tarefas
+        </button>
+    </div>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Adicione a sua tarefa</h5>
+                </div>
+                <div class="modal-body">
+                    <form action="adicionar-tarefa.php" method="POST">
+                        <!--input para adicionar tarefa-->
+                        <input type="text" class="form-control form-control-lg" name="nome" require />
+                        <div class="modal-footer display-flex ml-5">
+                            <input type="submit" class="btn btn-primary" value="Adicionar tarefa">
+
+                        </div>
+                    </form>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
