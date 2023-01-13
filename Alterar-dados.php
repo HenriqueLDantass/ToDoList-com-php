3f7bf5a646d43f1ff95d962e658e29ef1931debd
Henrique Dantas <henrique543@gmail.com>
2023-01-13 13:17:50 -0300

    criado a interface para alteração de dados

---
+++ Alterar-dados.php
@@ -0,0 +1,62 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body style="background-color: red;">
+
+    <section class="gradient-custom">
+        <div class="container py-4 h-50">
+            <div class="row d-flex justify-content-center align-items-center">
+                <div class="col-12 col-md-7 col-lg-6 col-xl-5">
+                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                        <div class="card-body p-3 text-center">
+
+                            <div class="mb-md-5 mt-md-4 pb-5">
+                                <div class="position-absolute">
+                                    <a class="nav-link text-white me-3 fs-5" href="voltar.php" role="button"><img src="Images/de-volta.png" alt=""></a>
+                                </div>
+                                <h2 class="fw-bold mb-2 text-uppercase">Alterar</h2>
+                                <div class="form-outline form-white mb-4">
+                                    <label class="form-label" for="typeEmailX">Nome</label>
+                                    <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+
+
+                                    <div class="form-outline form-white mb-4">
+                                        <label class="form-label" for="typeEmailX">Email</label>
+                                        <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+
+                                    </div>
+
+                                    <div class="form-outline form-white mb-4">
+                                        <label class="form-label" for="typePasswordX">Password</label>
+                                        <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+
+                                    </div>
+
+                                    <button class="btn btn-outline-light btn-lg px-5" type="submit">alterar</button>
+
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </section>
+
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -18,46 +18,28 @@ require('Config.php');
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <!-- Container wrapper -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <!-- Collapsible wrapper -->
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Navbar brand -->
+            <div class="collapse navbar-collapse">
                 <a class="navbar-brand mt-2 mt-lg-0" href="#">
                     TodoList
                 </a>
-                <!-- Left links -->
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Alterar dados</a>
+                        <a class="nav-link" href="Alterar-dados.php">Alterar dados</a>
                     </li>
                 </ul>
-                <!-- Left links -->
             </div>
-            <!-- Collapsible wrapper -->
-
-            <!-- Right elements -->
             <div class="d-flex align-items-center">
-                <!-- Icon -->
+
                 <a class="text-reset me-3" href="#">
                     <i class="fas fa-shopping-cart"></i>
                 </a>
-
-                <!-- Notifications -->
                 <div>
-
-                    <a class="text-reset me-3" href="sair.php" id="navbarDropdownMenuLink" role="button" data-mdb-toggle="dropdown" aria-expanded="false">
-                        Sair
-                    </a>
-
+                    <a class="nav-link text-white me-3 fs-5" href="sair.php" role="button">Sair</a>
                 </div>
-
-
             </div>
-            <!-- Right elements -->
         </div>
-        <!-- Container wrapper -->
     </nav>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
